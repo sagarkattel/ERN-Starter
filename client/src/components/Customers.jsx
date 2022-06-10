b79f09f6cca38e5ce82b11d58acbb1cf99e00616
sagarkattel <71958289+sagarkattel@users.noreply.github.com>
2022-06-10 16:01:56 +0545

    Update Customers.jsx

--- client/src/components/Customers.jsx
+++ client/src/components/Customers.jsx
@@ -1,25 +1,32 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
 
 function Customers() {
-    const [customers, setCustomers] = useState([]);
+  const [customers, setCustomers] = useState(null);
 
-    useEffect(() => {
-        fetch("/api/customers")
-            .then(res => res.json())
-            .then(customers => setCustomers(customers))
-    },[])
+  useEffect(() => {
+    fetch("https://second-expressapp.herokuapp.com/api/customers")
+      .then((res) => res.json())
+      .then((customers) => setCustomers(customers));
+  }, []);
 
-    return (
-        <div>
-            <h2>Customers</h2>
-            <ul>
-                {customers.map((customer) => {
-                    return <li key={customer.id}>{customer.firstName} {customer.lastName}</li>
-                })}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Customers</h2>
+      {!customers ? (
+        <h2>Loading customers...</h2>
+      ) : (
+        <ul>
+          {customers.map((customer) => {
+            return (
+              <li key={customer.id}>
+                {customer.firstName} {customer.lastName}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Customers;
